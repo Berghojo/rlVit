b54f2e0e9b413e20f05d2d880806a0907669d962
Joshua <berghoff.joshua@gmail.com>
2024-09-11 07:15:49 +0200

    Debug Off

--- train.py
+++ train.py
@@ -23,8 +23,8 @@ def setup(rank, world_size):
     os.environ['MASTER_ADDR'] = 'localhost'
     os.environ['MASTER_PORT'] = '12355'
 
-    os.environ["TORCH_CPP_LOG_LEVEL"] = "NONE"
-    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "NONE"
+    os.environ["TORCH_CPP_LOG_LEVEL"] = "OFF"
+    os.environ["TORCH_DISTRIBUTED_DEBUG"] = "OFF"
     # initialize the process group
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
 
