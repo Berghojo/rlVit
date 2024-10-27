8007112c89012b579c28510abdd2ca92aba1f4a2
Joshua <berghoff.joshua@gmail.com>
2024-10-27 13:28:36 +0100

    removed rnn

--- agent.py
+++ agent.py
@@ -15,7 +15,7 @@ class SingleActionAgent(nn.Module):
         self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding="same")
         self.fc = nn.Linear(35 * 35 * 32, 256)
         self.fc2 = nn.Linear(256, 256)
-        self.action = nn.RNNCell(256, n_patches+1)
+        self.action = nn.Linear(256, n_patches+1)
         self.value = nn.Linear(256, 1)
         self.relu = nn.ReLU()
         self._reset_parameters()
@@ -35,7 +35,7 @@ class SingleActionAgent(nn.Module):
         x = x.flatten(1)
         x = self.relu(self.fc(x))
         x = self.relu(self.fc2(x))
-        action = self.action(x, hidden)
+        action = self.action(x)
         value = self.value(x)
         return action, value, action.detach()
 
