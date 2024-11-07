34f0e368c9418c18ce5f1468e87972452950063b
Joshua <berghoff.joshua@gmail.com>
2024-11-07 23:46:26 +0100

    initial log

--- agent.py
+++ agent.py
@@ -13,14 +13,13 @@ class SingleActionAgent(nn.Module):
         super(SingleActionAgent, self).__init__()
         self.conv = nn.Conv2d(3, 16, kernel_size=5, stride=1, padding="same")
         self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding="same")
-        self.fc = nn.Linear(35 * 35 * 32, 256)
-        self.fc2 = nn.Linear(256, 256)
-        self.lstm = nn.LSTMCell(256, 128)
+        self.fc = nn.Linear(35 * 35 * 32, 128)
+        self.lstm = nn.LSTMCell(128, 128)
         self.action = nn.Linear(128, n_patches+1)
         self.value = nn.Linear(128, 1)
         self.relu = nn.ReLU()
         self._reset_parameters()
-        self.dout = nn.Dropout(p=0.5)
+        self.dout = nn.Dropout(p=0.2)
 
     def _reset_parameters(self):
         for p in self.parameters():
@@ -37,12 +36,12 @@ class SingleActionAgent(nn.Module):
             torch.zeros(batch_size, self.lstm.hidden_size, requires_grad=self.training)
         )
     def forward(self, x: Tensor, hidden) -> Tensor:
-        x = self.dout(self.conv(x))
-        x = self.dout(self.conv_2(x))
-        x = self.relu(x.flatten(1))
+        x = self.relu(self.conv(x))
+        x = self.relu(self.conv_2(x))
+        x = x.flatten(1)
 
         x = self.relu(self.fc(x))
-        x = self.relu(self.fc2(x))
+
         h_0, c_0 = hidden
         x, c_x = hidden = self.lstm(x, (h_0.detach(), c_0.detach()))
         action = self.action(x)
