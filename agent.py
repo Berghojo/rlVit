b0138dec345fcb5aa3d576009a3e36ae3a1cb34c
Joshua <berghoff.joshua@gmail.com>
2024-11-08 11:38:46 +0100

    initial log

--- agent.py
+++ agent.py
@@ -6,16 +6,39 @@ from torch import Tensor, device, dtype
 from collections import namedtuple, deque
 
 import numpy as np
+class Manager(nn.Module):
+    def __init__(self, d):
+        super(Manager, self).__init__()
+        self.state_projection = nn.Linear(d, d)
+        self.lstm = nn.LSTMCell(d, d)
+        self.value = nn.Linear(d, 1)
+        self.relu = nn.ReLU
+
+
+    def freeze(self, freeze):
+        print("Setting Agent Training to: ", freeze)
+        for param in self.parameters():
+            param.requires_grad = freeze
+
+    def forward(self, x, hidden=None):
+        x = self.relu(self.state_projection(x))
+        h, c = hidden
+        goal, _ = hidden = self.rnn(x, (h.detach, c.detach))
+        goal = torch.nn.functional.normalize(goal)
+        value = self.value(goal)
+        return goal, value, hidden
 
 
 class SingleActionAgent(nn.Module):
     def __init__(self, n_patches):
         super(SingleActionAgent, self).__init__()
+        self.d = 256
         self.conv = nn.Conv2d(3, 16, kernel_size=5, stride=1, padding="same")
         self.conv_2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding="same")
         self.fc = nn.Linear(35 * 35 * 32, 128)
         self.lstm = nn.LSTMCell(128, 128)
         self.action = nn.Linear(128, n_patches+1)
+        self.manager = Manager(n_patches)
         self.value = nn.Linear(128, 1)
         self.relu = nn.ReLU()
         self._reset_parameters()
